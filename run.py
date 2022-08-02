885329085c94520866de038deccb8bf3cf5a160d
Joha-will <johawilsonatu@icloud.com>
2022-08-02 20:26:48 +0000

    i create a variable with an input for users to start the game and a function to validate the data enter by users

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+print("Hello and welcome to this game! \n")
+
+print("Enter y for Yes and n for No.")
+initial_question = input("Would you like to try this game? ").lower()
+
+
+def initial_val():
+    """
+    This is the initial validation so the user
+    can input y or n to start the game.
+    """
+    if initial_question != 'y' or 'n':
+        print("Invalid data entered, please enter valid data!")
