b657fa33911700c1cf30d5c961d1702d3e7997a6
Joha-will <johawilsonatu@icloud.com>
2022-08-04 02:44:55 +0000

    i have created a new function for users to create a username and checks that the username passes the validation checks

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ def initial_val():
     if initial_question == 'y':
         print("Game loading... \n")
     elif initial_question == 'n':
+        print("GoodBye for now :)")
         quit()
     else:
         print("Invalid data entered, please enter 'y' if Yes or 'n' if No")
@@ -23,10 +24,28 @@ def initial_val():
 
 
 def create_user_name():
-    
+    """
+    This function would ask the user to enter a username
+    5 characters long.
+    """
+    while True:
+        try:
+            user_name = input("Enter a username you would like: ")
 
+            if len(user_name) < 5:
+                raise ValueError
+            elif not user_name[0].isupper():
+                raise ValueError
+            elif not user_name.isalnum():
+                raise ValueError
+            else:
+                print("Username is valid! :)")
+                break
 
-
+        except ValueError:
+            print("Please enter valid username.")
+            continue
 
 
 initial_val()
+create_user_name()
